f1649d58a4f27f3a4e210b25b2b933006fc11e57
owen <owen@sudorandom.org>
2023-08-24 17:36:21 -0700

    add interface

--- src/Akolytes.sol
+++ src/Akolytes.sol
@@ -3,13 +3,15 @@ pragma solidity 0.8.20;
 
 import {ERC721} from "solmate/tokens/ERC721.sol";
 import {ERC2981} from "openzeppelin-contracts/contracts/token/common/ERC2981.sol";
+import {IERC2981} from "openzeppelin-contracts/contracts/interfaces/IERC2981.sol";
+import {ILSSVMPairFactoryLike} from "./ILSSVMPairFactory.sol";
 
 contract Akolytes is ERC721, ERC2981 {
 
     address immutable MONS;
     address immutable SUDO_FACTORY;
 
-    // Mapping of (token address | id) => amount claimed
+    // Mapping of (token address, 160 bits | id, 90 bits) => amount already claimed
     mapping(uint256 => uint256) public royaltyClaimedPerId;
 
     // Mapping of amount accumulated in total per token address
@@ -21,6 +23,23 @@ contract Akolytes is ERC721, ERC2981 {
 
         // 1% royalty, set to this address
         _setDefaultRoyalty(address(this), 100);
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 id
+    ) public override {
+        super.transferFrom(from, to, id);
+    }
+
+    // Overrides both
+    function supportsInterface(bytes4 interfaceId) public view override(ERC2981, ERC721) returns (bool) {
+        return
+            interfaceId == 0x01ffc9a7 || // ERC165 Interface ID for ERC165
+            interfaceId == 0x80ac58cd || // ERC165 Interface ID for ERC721
+            interfaceId == type(IERC2981).interfaceId || // ERC165 interface for IERC2981
+            interfaceId == 0x5b5e139f; // ERC165 Interface ID for ERC721Metadata
     }
 
     function tokenURI(uint256 id) public view override returns (string memory) {
