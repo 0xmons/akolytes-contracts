7d1be76da2be8e476ad82296f0a0302b86b7cc35
owen <owen@sudorandom.org>
2023-10-26 23:26:12 -0700

    new dep

--- script/deploy.s.sol
+++ script/deploy.s.sol
@@ -11,14 +11,22 @@ contract MyScript is Script {
     function run() external returns (Markov m, Test20 t, Akolytes a) {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
         vm.startBroadcast(deployerPrivateKey);
-        m = new Markov();
-        t = new Test20();
+        // m = new Markov();
+        // t = new Test20();
+        // a = new Akolytes(
+        //     address(0),
+        //     0x967544b2Dd5c1c7A459e810C9B60AE4FC8227201,
+        //     address(m),
+        //     0x5e9a0Ef66A6BC2E6Ac7C9811374521f7BAd89e53,
+        //     address(t),
+        //     0x9fe1E403c043214017a6719c1b64190c634229eF
+        // );
         a = new Akolytes(
-            address(0),
+            0x0000000000000000000000000000000000000000,
             0x967544b2Dd5c1c7A459e810C9B60AE4FC8227201,
-            address(m),
+            0x3d126031A109a93bC6D80F04Ba5684A0BdD9BE1b,
             0x5e9a0Ef66A6BC2E6Ac7C9811374521f7BAd89e53,
-            address(t),
+            0x9B2660A7BEcd0Bf3d90401D1C214d2CD36317da5,
             0x9fe1E403c043214017a6719c1b64190c634229eF
         );
         vm.stopBroadcast();
