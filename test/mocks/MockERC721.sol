3c43bda573b76b6072d60c18334715ce0497cdb2
owen <owen@sudorandom.org>
2023-11-01 12:42:07 -0700

    add test

--- test/mocks/MockERC721.sol
+++ test/mocks/MockERC721.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.20;
 
-import {ERC721} from "solmate/tokens/ERC721.sol";
+import {ERC721Enumerable} from "openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {ERC721} from "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
-contract MockERC721 is ERC721 {
+contract MockERC721 is ERC721Enumerable {
+
     constructor() ERC721('A', 'A') {}
 
     function mint(uint256 offset, uint256 amount) public {
@@ -12,7 +14,6 @@ contract MockERC721 is ERC721 {
         }
     }
 
-    // Handles metadata from arweave hash
     function tokenURI(uint256) public pure override returns (string memory) {
         return "";
     }
